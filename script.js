480fbf144a8faf6fb3e4f86eb0f6ad74cdc64388
Tanya <106194959+tanya95gordeeva@users.noreply.github.com>
2022-11-01 22:50:21 +0400

    final html code
    for full hd screen

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 'use strict';
 
+// MAIN VARIABLES
+const navItems = document.querySelectorAll('.nav__link');
+
 console.log('Script is working');
+
+// SCROLLING FROM NAV BAR
+
+navItems.forEach((item) => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
