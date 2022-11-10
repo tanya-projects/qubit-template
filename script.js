39014b10412bb5959b4db41e47cdc65d1fad8c7c
Tanya <106194959+tanya95gordeeva@users.noreply.github.com>
2022-11-10 16:44:55 +0400

    adaptive styles

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 // MAIN VARIABLES
 const navItems = document.querySelectorAll('.nav__link');
+const openMenu = document.querySelector('.menu__btn');
+const navbar = document.querySelector('.header__nav-items');
 
 console.log('Script is working');
 
@@ -13,5 +15,22 @@ navItems.forEach((item) => {
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth',
     });
+    if (openMenu.classList.value.includes('open')) {
+      navbar.style.left = '-100vw';
+      openMenu.classList.remove('open');
+    }
+    console.log(openMenu.classList.value.includes('open'));
   });
+});
+
+// OPEN/CLOSE MENU BAR ON MOBILE DEVICES
+openMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (!openMenu.classList.value.includes('open')) {
+    navbar.style.left = 0;
+    openMenu.classList.add('open');
+  } else {
+    navbar.style.left = '-100vw';
+    openMenu.classList.remove('open');
+  }
 });
